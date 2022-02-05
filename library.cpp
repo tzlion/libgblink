0aa537e509348ad9a7e08ff6bc5451e4b86e3556
taizou <cactusgunman@gmail.com>
2022-02-05 00:01:11 +0000

    implement gblink as library

---
+++ library.cpp
@@ -0,0 +1,236 @@
+/*
+  SIMPLIFIED GBLINK IMPLEMENTATION
+  Based on: original gblinkdl by Brian Provinciano
+  Modified gblinkdx by taizou
+  (this file is not strictly GPL it's just whatever sorry)
+*/
+
+#include "library.h"
+
+#include "stdio.h"
+#include "string.h"
+
+#include "windows.h"
+
+using namespace std;
+
+U8 bank0[0x4000];
+bool linkerActive = false;
+bool linkerInitialising = false;
+LinkerLogger logger = nullptr;
+HINSTANCE hInpOutDll;
+
+typedef void(__stdcall *lpOut32)(short, short);
+typedef short(__stdcall *lpInp32)(short);
+lpOut32 gfpOut32;
+lpInp32 gfpInp32;
+
+char msg[420];
+
+void logMessage(const char* message) {
+    if (logger) {
+        logger(message);
+    }
+}
+
+unsigned char inportb(unsigned short port)
+{
+    return gfpInp32(port);
+}
+
+void outportb(unsigned short port, unsigned char value)
+{
+    gfpOut32(port,value);
+}
+
+void lptdelay(int amt)
+{
+    for(int i=0;i<amt;i++)
+        inportb(LPTREG_DATA);
+}
+
+U8 gb_sendbyte(U8 value)
+{
+    U8 read = 0;
+    for(int i=7;i>=0;i--) {
+        U8 v = (value>>i)&1;
+
+        outportb(LPTREG_DATA, v|D_CLOCK_HIGH);
+        outportb(LPTREG_DATA, v);
+
+        U8 stat = inportb(LPTREG_STATUS);
+
+        if(!(stat&STATUS_BUSY))
+            read |= (1<<i);
+
+        outportb(LPTREG_DATA, v|D_CLOCK_HIGH);
+    }
+    lptdelay(64);
+    return read;
+}
+
+U8 gb_readbyte()
+{
+    U8 read = 0;
+    for(int i=7;i>=0;i--) {
+        outportb(LPTREG_DATA, D_CLOCK_HIGH);
+        outportb(LPTREG_DATA, 0);
+
+        if(!(inportb(LPTREG_STATUS)&STATUS_BUSY))
+            read |= (1<<i);
+        outportb(LPTREG_DATA, D_CLOCK_HIGH);
+    }
+    // delay between bytes
+    lptdelay(64);
+    return read;
+}
+
+void gb_sendwrite(U16 addr, U8 val)
+{
+    gb_sendbyte(0x49);
+    gb_sendbyte(addr>>8);
+    gb_sendbyte(addr&0xFF);
+    gb_sendbyte(val);
+}
+
+void gb_sendblockread(U16 addr, U16 length)
+{
+    gb_sendbyte(0x59);
+    gb_sendbyte(addr>>8);
+    gb_sendbyte(addr&0xFF);
+    gb_sendbyte(length>>8);
+    gb_sendbyte(length&0xFF);
+}
+
+void GBLINK_CALL readBlock(U8 *dest, U16 addr, int len)
+{
+    logMessage(msg);
+    gb_sendblockread(addr,len);
+    for(int i=0;i<len;i++)
+        dest[i] = gb_readbyte();
+}
+
+U8 GBLINK_CALL readByte(U16 addr)
+{
+    gb_sendblockread(addr,1);
+    return gb_readbyte();
+}
+
+void GBLINK_CALL writeByte(U16 addr, U8 val)
+{
+    gb_sendwrite(addr, val);
+}
+
+void readBankZero()
+{
+    // read the first bank of ROM
+    logMessage("Downloading first bank...");
+    for(int i=0;i<0x4000;i++) {
+        bank0[i] = gb_readbyte();
+    }
+}
+
+bool GBLINK_CALL initLinker()
+{
+    if (linkerInitialising) {
+        logMessage("Already initialising");
+        return false;
+    }
+
+    linkerInitialising = true;
+
+    hInpOutDll = LoadLibrary("inpout32.dll");
+    if (hInpOutDll != NULL) {
+        gfpOut32 = (lpOut32)GetProcAddress(hInpOutDll, "Out32");
+        gfpInp32 = (lpInp32)GetProcAddress(hInpOutDll, "Inp32");
+    } else {
+        logMessage("Unable to load inpout32.dll");
+        return false;
+    }
+
+    logMessage("GBlinkDX client adaptation for hhugboy");
+    logMessage("Based on original GBlinkdl by Brian Provinciano & GBlinkDX by taizou");
+
+    logMessage("Setting up ports...");
+
+    // set up the parallel port
+    outportb(LPTREG_CONTROL, inportb(LPTREG_CONTROL)&(~CTL_MODE_DATAIN));
+    outportb(LPTREG_DATA, 0xFF);
+    outportb(LPTREG_DATA, D_CLOCK_HIGH);
+
+    // perform communication
+    logMessage("Waiting for Game Boy...");
+    while(gb_sendbyte(0x9A)!=0xB4) {}
+    lptdelay(2000);
+    if(gb_sendbyte(0x9A)!=0x1D) {
+        logMessage("Bad connection");
+        return false;
+    }
+
+    logMessage("Connected.");
+
+    U8 carttype = gb_readbyte();
+    U8 romsize = gb_readbyte();
+    U8 ramsize = gb_readbyte();
+    U16 checksum = (gb_readbyte()<<8) | (gb_readbyte());
+
+    char gamename[17];
+    for(int i=0;i<16;i++)
+        gamename[i] = gb_readbyte();
+    gamename[16] = '\0';
+
+    sprintf(msg,
+            "Cartridge header: Title %s / Cart type %02X / ROM size %02X / RAM size %02X / Checksum %04X",
+            gamename,carttype,romsize,ramsize,checksum
+    );
+    logMessage(msg);
+
+    if(gb_readbyte() != 0) {// verify we're done
+        logMessage("expected 0x00 from GB, bad connection");
+        return false;
+    }
+    if(gb_readbyte() != 0xFF) {// verify we're done
+        logMessage("expected 0xFF from GB, bad connection");
+        return false;
+    }
+
+    logMessage("Reading first bank...");
+
+    readBankZero();
+
+    logMessage("Ready!");
+
+    linkerInitialising = false;
+    linkerActive = true;
+
+    return true;
+}
+
+void GBLINK_CALL deinitLinker() {
+    if (linkerInitialising) {
+        return;
+    }
+    linkerActive = false;
+    outportb(LPTREG_DATA, D_CLOCK_HIGH);
+    outportb(LPTREG_CONTROL, inportb(LPTREG_CONTROL)&(~CTL_MODE_DATAIN));
+    outportb(LPTREG_DATA, 0xFF);
+    FreeLibrary(hInpOutDll);
+    logMessage("Disconnected");
+}
+
+void GBLINK_CALL setLogger(LinkerLogger newlogger) {
+    logger = newlogger;
+    logMessage("Setup logger");
+}
+
+bool GBLINK_CALL isLinkerActive() {
+    return linkerActive;
+}
+
+bool GBLINK_CALL isLinkerInitialising() {
+    return linkerInitialising;
+}
+
+U8* GBLINK_CALL getBank0() {
+    return bank0;
+}
--- library.h
+++ library.h
@@ -1,30 +1,41 @@
-/* add.h
+#define GBLINK_EXPORTS
 
-   Declares the functions to be imported by our application, and exported by our
-   DLL, in a flexible and elegant way.
-*/
-
-/* You should define ADD_EXPORTS *only* when building the DLL. */
-#ifdef ADD_EXPORTS
-#define ADDAPI __declspec(dllexport)
+#ifdef GBLINK_EXPORTS
+#define GBLINK_API __declspec(dllexport)
 #else
-#define ADDAPI __declspec(dllimport)
+#define GBLINK_API __declspec(dllimport)
 #endif
 
-/* Define calling convention in one place, for convenience. */
-#define ADDCALL __cdecl
-
-/* Make sure functions are exported with C linkage under C++ compilers. */
+#define GBLINK_CALL __cdecl
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-/* Declare our Add function using the above definitions. */
-ADDAPI int ADDCALL Add(int a, int b);
+typedef unsigned char U8;
+typedef unsigned short U16;
 
+#define LPTREG_DATA 0x378
+#define LPTREG_STATUS (LPTREG_DATA + 1)
+#define LPTREG_CONTROL (LPTREG_DATA + 2)
+
+#define STATUS_BUSY 0x80
+#define CTL_MODE_DATAIN 0x20
+#define D_CLOCK_HIGH 0x02
+
+typedef void(*LinkerLogger)(const char*);
+
+GBLINK_API bool GBLINK_CALL initLinker();
+GBLINK_API void GBLINK_CALL deinitLinker();
+GBLINK_API void GBLINK_CALL readBlock(U8 * dest, U16 addr, int len);
+GBLINK_API U8 GBLINK_CALL readByte(U16 addr);
+GBLINK_API void GBLINK_CALL writeByte(U16 addr, U8 val);
+GBLINK_API void GBLINK_CALL setLogger(LinkerLogger newlogger);
+GBLINK_API bool GBLINK_CALL isLinkerActive();
+GBLINK_API bool GBLINK_CALL isLinkerInitialising();
+GBLINK_API U8* GBLINK_CALL getBank0();
 
 #ifdef __cplusplus
-} // __cplusplus defined.
+}
 #endif
